f456ec6ae66cedb51d9c206fa8fc6fabf1c4776d
Rishabh Oswal <rishabh.oswal@outlook.com>
2022-10-07 23:31:57 -0700

    School District DDL programmatic population
    Populate School District drop down list programmatically.

--- course_catalog.js
+++ course_catalog.js
@@ -1,9 +1,57 @@
-
+let school_districts_collection =
+    [
+        {
+            "school_district_id": "101",
+            "school_district_name": "Lake Washington School District"
+        },
+        {
+            "school_district_id": "102",
+            "school_district_name": "Issaquah School District"
+        },
+        {
+            "school_district_id": "103",
+            "school_district_name": "Riverview School District"
+        },
+        {
+            "school_district_id": "104",
+            "school_district_name": "Skykomish School District"
+        },
+        {
+            "school_district_id": "105",
+            "school_district_name": "Snoqualmie Valley School District"
+        },
+        {
+            "school_district_id": "106",
+            "school_district_name": "Northshore School District"
+        },
+        {
+            "school_district_id": "107",
+            "school_district_name": "Monroe School District"
+        },
+        {
+            "school_district_id": "108",
+            "school_district_name": "Snohomish School District"
+        },
+        {
+            "school_district_id": "109",
+            "school_district_name": "Lake Stevens School District"
+        },
+        {
+            "school_district_id": "110",
+            "school_district_name": "Arlington School District"
+        },
+        {
+            "school_district_id": "111",
+            "school_district_name": "Granite Falls School District"
+        },
+    ];
 
 let course_collection =
     [
         {
-            "school_id": "redmond",
+            "school_id": "1001",
+            "school_name": "Redmond High School",
+            "school_district_id": "101",
             "class9":
             {
                 "english":
@@ -63,7 +111,9 @@ let course_collection =
         },
 
         {
-            "school_id": "eastlake",
+            "school_id": "1002",
+            "school_name": "Eastlake High School",
+            "school_district_id": "101",
             "class9":
             {
                 "english":
